0490e3d2244fcef1ec08c4a4a424697265f8a575
Rifat <rifat@binate-solutions.com>
2022-03-01 18:15:22 +0600

    Changed structure

--- tutorial/tutorial/spiders/jokes.py
+++ tutorial/tutorial/spiders/jokes.py
@@ -8,13 +8,15 @@ class JokesSpider(scrapy.Spider):
     start_urls = ['http://www.laughfactory.com/jokes/family-jokes']
 
     def parse(self, response):
-        jokes_list=response.xpath("//div[@class='jokes']/div[@class='joke-msg']/div[@class='joke-text']/p/text()").extract()
+        # jokes_list=response.xpath("//div[@class='jokes']/div[@class='joke-msg']/div[@class='joke-text']/p[descendant-or-self::text()]").extract()
+        jokes_list=response.xpath("//div[@class='jokes']")
         count=0
         for joke in jokes_list:
             count+=1
             result={
                 'count':count,
-                'joke_text':joke.strip()
+                'joke_text':joke.xpath(".//div[@class='joke-msg']/div[@class='joke-text']/p/text()").extract()
+                # 'joke_text':joke.css("div.joke-msg div.joke-text p::text")
                 # 'joke_text':str(joke).strip().replace(r"\r","").replace(r"\n","")
             }
             print("Result--->",result)
