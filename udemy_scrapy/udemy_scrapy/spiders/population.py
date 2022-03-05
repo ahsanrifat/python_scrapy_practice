cb56ebda9bd8d5699311ea0c2e7fd58ad4a26f42
Syed Rifat Ahsan <frifat2013@gmail.com>
2022-03-05 12:40:18 +0600

    Added .gitignore file

--- udemy_scrapy/udemy_scrapy/spiders/population.py
+++ udemy_scrapy/udemy_scrapy/spiders/population.py
@@ -8,12 +8,16 @@ class CovidSpider(scrapy.Spider):
 
     def parse(self, response):
         country_block=response.xpath("//h2[normalize-space(text())='Population of Countries']/following::ul")[0].css("a")
-        for country in country_block:
-            ans={
-                # "country":country.css('a::text').extract(),
-                "country":country.xpath('.//text()').get(),
-                # "link":self.allowed_domains[0]+str(country.css('a::attr(href)').extract()[0])
-                "link":self.allowed_domains[0]+str(country.xpath(".//@href").get())
-            }
-            print("Data->",ans)
-            yield scrapy.Request(url=ans['link'])
+        try:
+            for country in country_block:
+                ans={
+                    # "country":country.css('a::text').extract(),
+                    "country":country.xpath('.//text()').get(),
+                    # "link":self.allowed_domains[0]+str(country.css('a::attr(href)').extract()[0])
+                    "link":self.allowed_domains[0]+str(country.xpath(".//@href").get())
+                }
+                print("Data->",ans)
+                yield ans
+                # yield scrapy.Request(url=ans['link'])
+        except Exception as e:
+            print("Exception-->",e)
